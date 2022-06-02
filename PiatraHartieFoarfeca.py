57fddf7b9ace4a8f537eff71a07a2cea3888900d
Andreea Groza <andreea.loredana@yahoo.com>
2022-06-02 22:50:36 +0300

    firstcommit

---
+++ PiatraHartieFoarfeca.py
@@ -0,0 +1,32 @@
+#def f1():
+print ("Hello1")
+f1()
+import utilities
+utilities.f1()
+def f1():
+	print GATA
+	print"hello1"
+	if name_==" main"
+		def factorial (a)
+			if a==0:
+				return 1
+			else
+				return a*factorial (a-1)
+			x=lambda a:a+10
+			print(x(5))
+			x=lambda a,b:a*b
+			print(x(5,6))
+
+
+			gretting= "Hello world"
+			gretting[0]='J'
+String1="Laborator"
+String2="Python"
+String3=String1+String2
+print(String3)
+String1="Python"+""+"Laborator"
+print(String1)
+String2=String1+"3"
+print(String2)
+
+
